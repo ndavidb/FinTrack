8cc98e07354a52e934c18d6c22008b2a2e30b012
ndavidb <bell0465@flinders.edu.au>
2024-06-21 13:31:49 +0930

    Middleware bug fixed and routes defined

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,23 +1,20 @@
 'use client';
 
 import Searchbar from "@/components/Search/Searchbar";
-import React, {ChangeEvent, SyntheticEvent, useState} from "react";
+import React, {ChangeEvent, SyntheticEvent, useEffect, useState} from "react";
 import HomePortfolioReview from "@/components/UserHomePortfolio/HomePortfolioReview";
 import SearchResultList from "@/components/Search/SearchResultList";
 import {searchCompanies} from "@/lib/data";
 import {Button} from "@/components/ui/button";
 import SearchStocks from "@/components/Search/SearchStocks";
-import {useRouter} from "next/router";
 
-interface Props {
-}
 
-export default function HomeUser({}: Props) {
-    const isUserAuthenticated : Boolean = false;
+export default function HomeUser() {
     const [search, setSearch] = useState<string>("");
     const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
     const [serverError, setServerError] = useState<string | null>(null);
-
+    
+    
     const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) =>{
         setSearch(e.target.value);
     }
---
+++ client/middleware.ts
@@ -0,0 +1,32 @@
+﻿import {NextResponse} from "next/server";
+import {NextRequest} from "next/server";
+import {parseCookies} from "nookies";
+
+export function middleware(request: NextRequest) {
+
+    const token = request.cookies.get('token')?.value;
+
+    const { pathname } = request.nextUrl
+
+    if (pathname.includes('/home') && !token) {
+        // Redirect to login if no token is found
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+
+    if (pathname === '/login' && token) {
+        // Redirect to homepage if user is already logged in
+        return NextResponse.redirect(new URL('/home', request.url))
+    }
+    
+    if (pathname === '/register' && token) {
+        return NextResponse.redirect(new URL('/home', request.url))    
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/home', '/login', '/register']
+}
+
+

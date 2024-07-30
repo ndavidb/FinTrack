ec65d313e93cfa4469431c348e924c33e1898fc7
ndavidb <bell0465@flinders.edu.au>
2024-07-30 20:56:18 +0930

    Migrations considering last changes to the database

---
+++ api/Migrations/20240730095902_lastChange.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class lastChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "54d8ed83-2973-4421-bd72-5617fc43862e");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5d3b1223-50f1-4201-a415-568fcee2c1a1");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "4bb9e77e-45a2-4efc-8d0f-725beeac5907", null, "User", "USER" },
+                    { "b2ddd3d5-341c-4aec-8bc3-16972dee7d16", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4bb9e77e-45a2-4efc-8d0f-725beeac5907");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b2ddd3d5-341c-4aec-8bc3-16972dee7d16");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "54d8ed83-2973-4421-bd72-5617fc43862e", null, "Admin", "ADMIN" },
+                    { "5d3b1223-50f1-4201-a415-568fcee2c1a1", null, "User", "USER" }
+                });
+        }
+    }
+}

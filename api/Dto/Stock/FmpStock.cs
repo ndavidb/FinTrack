29895cc51ea5e4d3392728036a4c86d9818c818f
ndavidb <bell0465@flinders.edu.au>
2024-07-31 15:49:43 +0930

    Changed FmpStock class to be a public class instead of a public abstract class to be able to be instantiated.

--- api/Dto/Stock/FmpStock.cs
+++ api/Dto/Stock/FmpStock.cs
@@ -1,39 +1,39 @@
 ﻿namespace api.Dto.Stock;
 
-public abstract class FmpStock
+public class FmpStock
 {
     public FmpStock() {}
-    public string symbol { get; set; } = null!;
+    public string symbol { get; set; } = string.Empty;
     public double price { get; set; }
     public double beta { get; set; }
     public int volAvg { get; set; }
     public long mktCap { get; set; }
     public double lastDiv { get; set; }
-    public string range { get; set; } = null!;
+    public string range { get; set; } = string.Empty;
     public double changes { get; set; }
-    public string companyName { get; set; } = null!;
-    public string currency { get; set; } = null!;
-    public string cik { get; set; } = null!;
-    public string isin { get; set; } = null!;
-    public string cusip { get; set; } = null!;
-    public string exchange { get; set; } = null!;
-    public string exchangeShortName { get; set; } = null!;
-    public string industry { get; set; } = null!;
-    public string website { get; set; } = null!;
-    public string description { get; set; } = null!;
-    public string ceo { get; set; } = null!;
-    public string sector { get; set; } = null!;
-    public string country { get; set; } = null!;
-    public string fullTimeEmployees { get; set; } = null!;
-    public string phone { get; set; } = null!;
-    public string address { get; set; } = null!;
-    public string city { get; set; } = null!;
-    public string state { get; set; } = null!;
-    public string zip { get; set; } = null!;
+    public string companyName { get; set; } = string.Empty;
+    public string currency { get; set; } = string.Empty;
+    public string cik { get; set; } = string.Empty;
+    public string isin { get; set; } = string.Empty;
+    public string cusip { get; set; } = string.Empty;
+    public string exchange { get; set; } = string.Empty;
+    public string exchangeShortName { get; set; } = string.Empty;
+    public string industry { get; set; } = string.Empty;
+    public string website { get; set; } = string.Empty;
+    public string description { get; set; } = string.Empty;
+    public string ceo { get; set; } = string.Empty;
+    public string sector { get; set; } = string.Empty;
+    public string country { get; set; } = string.Empty;
+    public string fullTimeEmployees { get; set; } = string.Empty;
+    public string phone { get; set; } = string.Empty;
+    public string address { get; set; } = string.Empty;
+    public string city { get; set; } = string.Empty;
+    public string state { get; set; } = string.Empty;
+    public string zip { get; set; } = string.Empty;
     public double dcfDiff { get; set; }
     public double dcf { get; set; }
-    public string image { get; set; } = null!;
-    public string ipoDate { get; set; } = null!;
+    public string image { get; set; } = string.Empty;
+    public string ipoDate { get; set; } = string.Empty;
     public bool defaultImage { get; set; }
     public bool isEtf { get; set; }
     public bool isActivelyTrading { get; set; }
--- api/Services/FmpService.cs
+++ api/Services/FmpService.cs
@@ -3,7 +3,6 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using api.Dto.Stock;
 using api.Interfaces;
-using api.Mappers;
 using api.Models;
 
 namespace api.Services;

f0319264f5f72824a921037de85f4011143ea91a
ndavidb <bell0465@flinders.edu.au>
2024-07-31 14:48:19 +0930

    Fixed bug with statementTable.tsx

--- client/components/StatementTable/statementTable.tsx
+++ client/components/StatementTable/statementTable.tsx
@@ -1,5 +1,4 @@
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
-import {TestDataCompany} from "@/components/StatementTable/TestData";
 
 interface Props {
     config: any;
